0f1fd60f80e800d7f9171b55f152ef1a0fab8671
EngetoStudent <engeto.student@gmail.com>
2024-07-03 19:04:33 +0200

    new file:   DU1/project_1_pokus .py new file:   Engeto_lektor.py modified:   Ukol_1/__pycache__/task_template.cpython-312.pyc modified:   Ukol_1/project_1.py

--- Ukol_1/project_1.py
+++ Ukol_1/project_1.py
@@ -7,7 +7,7 @@ discord: jankasparek0720
 # import the task_template.py (needs to be in poject folder) as task 
 import task_template as task
 
-#Credentials input
+#Users definitions by dict 
 users = {0:
             {"name":"bob",
             "password":"123"},
@@ -21,21 +21,24 @@ users = {0:
             {"name":"liz",
             "password":"pass123"},
         }
-    
-index = 1             
+#Index for while     
+index = 1  
+           
 while index == 1:
-
+#Login inputs by user 
     login_name = input("\nInsert user name: ")
     login_password = input("Inser password: " )
 
+#For block which compares all def. users with input  
     for i in users:
+    #If the input is correct, set index for the while loop to 0 and break from the block  
         if login_name == users[i]["name"] and login_password == users[i]["password"]:
             print("*"*40)
             print("Welcome to the app,", login_name ,"\nWe have 3 texts to be analyzed.")
             print("*"*40)
             index = 0
             break
-
+    #If the user name is correct but the password is wrong, user can try again, index is still 1, so after the break, while loop will start again 
         elif login_name == users[i]["name"] and login_password != users[i]["password"]:
             print("*"*25)
             print("wrong password, try again")
@@ -44,6 +47,7 @@ while index == 1:
 
         else:
             continue
+#If name and password is incorrect program will end 
     else:
         print("*"*44)
         print("Unregistered user, terminating the program..")
@@ -53,16 +57,18 @@ while index == 1:
 def text_analyze():   
 #Coded in Try/Exept condition if input is not text
     try:
-        text_number = int(input("Enter a number btw. 1 and 3 to select: "))
-        print("You choose text n.",text_number)
+    #Takes a number of texts in task.TEXTS list 
+        number_of_texts = len(task.TEXTS)
+        print("Enter a number btw. 1 and", number_of_texts , "to select: ")
+        text_number = int(input("Choose a number:"))
+        print("You choose text n.", text_number)
         print("*"*40) 
 
-    #Allow only 1 - 3 input 
-        if text_number >= 1 and text_number <= len(task.TEXTS):
+    #Allow only input from 1 to number of the texts
+        if text_number >= 1 and text_number <= number_of_texts:
         #Take the specific text from tuple in task 
             chosen_text = task.TEXTS[text_number - 1] # -1 because tuple index starts from 0 
 
-        #Number of words 
         #Takes the whole text and splits it to separate words
             split_of_words = chosen_text.split()
             number_of_words = len(split_of_words)
@@ -84,44 +90,38 @@ def text_analyze():
         #Special symbols to be removed 
             symbols = [",",".","?","!","@","#","$","%","&","/","|"]
 
-            for i in split_of_words: 
-                
+            for i in split_of_words:                
             #Titlecase words
                 tittlecase = str(i)
                 if tittlecase[0].isupper():
                     a=a+1 
                 else:
                     a=a+0  
-
             #Uppercase words
                 uppercase = str(i)
                 if uppercase.isupper() and not uppercase[0].isdigit(): #exlude uppercase words with digits in it 
                     b=b+1 
                 else:
-                    b=b+0
-        
+                    b=b+0        
             #Lowercase words
                 lowercase = str(i)
                 if lowercase.islower() and not lowercase[0].isdigit():# Exclude lovercase words with digits in it
                     c=c+1 
                 else:
                     c=c+0
-
             #Numeric words
                 digit = str(i)
                 if digit.isnumeric():
                     d=d+1 
                     sum = sum+int(i)
                 else:
-                    d=d+0
-            
+                    d=d+0            
             # Block for remowing special symbols from letters 
                 for j in symbols:
             #take each word from split words and removes symbol
                     i = i.replace(j,"")
             #Cleared words insert into list     
                 clear_words.append(i)
-
 
             print("There are",a ,"titlecase words.")
             print("There are",b ,"uppercase words.")
@@ -133,7 +133,7 @@ def text_analyze():
         else:
             print("Wrong selection of text, terminating the program..")
             quit()
-        
+            
     #Block of code which puts lenght of cleared words and their count into a dict 
         for i in clear_words:
             lenght = len(i)
