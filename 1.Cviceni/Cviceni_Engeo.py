b35a2a10a094e32a9a216a57001adb57736c4101
EngetoStudent <engeto.student@gmail.com>
2024-07-03 18:07:31 +0200

    modified:   Ukol_1/__pycache__/task_template.cpython-312.pyc modified:   Ukol_1/project_1.py

---
+++ 1.Cviceni/Cviceni_Engeo.py
@@ -0,0 +1,347 @@
+"""
+list = ["Frantisek", "Bruno", "Anna", "Jakub", "Klara", "Anezka", "Anezka", "Anezka"]
+posledni_index = len(list)-1
+print(list[posledni_index])
+
+print("Na indexu 2 je:", list[2])
+print("Na indexu 7 je:", list[7])
+print("V intervalu od 2 do 5 je:", list[2:6])
+print("Každý třetí člen je :", list[::3])
+"""
+"""
+#palindrom
+zadana_slova = ["jaro", "jej", "kolo", "aha"]
+slovo = zadana_slova[1]
+palindrom = slovo[::-1]
+
+if slovo == palindrom:
+    print("slovo", slovo, "je palindrom")
+else:
+    print("slovo", slovo, " neni palindrom")
+"""
+
+"""
+zamestnanci = ["Frantisek", "Anna", "Jakub", "Klara"]
+print("Zamestnanci na zacatku:", zamestnanci)
+
+zamestnanci_a = zamestnanci.copy()
+zamestnanci_a.append("Bruno")
+zamestnanci_a.append("Anezka")
+print("Nova jmena pridana:", zamestnanci_a)
+
+zamestnanci_b = zamestnanci_a.copy()
+zamestnanci_b.insert(1, "Bruno")
+print("Nova jmena vlozena:", zamestnanci_b)
+"""
+
+"""
+vstupni_cisla = [1, 2, 3, 4, 5, 6, 7]
+vstupni_pismena = ["p", "ú", "s", "č", "p", "s", "n"]
+tyden = ('pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota', 'neděle')
+
+cislo_dne = 3
+
+if cislo_dne in vstupni_cisla:
+    print("Správná vstupní hodnota!")
+    den_tydne = tyden[cislo_dne - 1]
+    if den_tydne[0] == vstupni_pismena[cislo_dne-1]:
+        print("Správné písmeno")
+    else:
+        print("Špatné písmeno")
+else:
+    print("Špatná vstupní hodnota!")    
+"""
+"""
+kandidati = ["Bruno", "Anežka"]
+zamestnanci = ['František', 'Bruno', 'Anna', 'Jakub', 'Klára']
+
+bez_bruna = kandidati.copy()
+bez_bruna.remove("Bruno")
+print("Bruno odstraněn:", bez_bruna)
+
+opakovani_kandidati = 3*bez_bruna
+print("Opakování kandidáti:", opakovani_kandidati)
+
+spojeni_zamestnanci = zamestnanci + opakovani_kandidati
+print("Spojení zaměstnanci:", spojeni_zamestnanci)
+"""
+"""
+#BMI Karkulacka
+vyska = 2
+vaha = 80
+jmeno = 'Martin'
+
+bmi = vaha / vyska ** 2
+if bmi > 40:
+    kategorie = 'těžká obezita'
+elif bmi > 30:
+    kategorie = 'obezita'
+elif bmi > 25:
+    kategorie = 'mírná nadváha'
+elif bmi > 18.5:
+    kategorie = 'zdravá váha'
+else:
+    kategorie = 'podvýživa'
+
+print(jmeno, "tvé BMI je", bmi, ", což spadá do kategorie", kategorie, ".")
+"""
+
+"""
+zadana_slova = ["Matous", "Martin", "ahoj","er", "es", "i", "a"]
+slovo = zadana_slova[1]
+delka_slova = len(slovo)
+
+if delka_slova >= 4:
+    print(slovo, "počet znaků:", delka_slova)
+elif delka_slova > 3 and delka_slova < 1:
+    print(slovo, "počet znaků: 2")
+elif delka_slova == 1:
+    print(slovo, "počet znaků:1")
+"""
+"""
+#Sety 
+cisla_1 = (1, 1, 2, 3, 4)
+cisla_2 = (5, 6, 7, 7, 8)
+cisla_1 = set(cisla_1)
+cisla_2 = set(cisla_2)
+sjednocene_hodnoty = cisla_1.union(cisla_2)
+print("Sjednocené hodnoty ze zadání:", sjednocene_hodnoty)
+"""
+"""
+cisla_1 = {1, 2, 3, 4}
+cisla_2 = {3, 4, 5, 6}
+rozdil_cisel = cisla_1.difference(cisla_2)
+print("Rozdílné hodnoty prvního setu oproti druhému:", rozdil_cisel)
+"""
+
+"""
+##Dictionary
+user_1 = {}
+user_email = {"email":"marek.parek@gamil.com"}
+user_1['name'] = "Marek"
+user_1['surname'] = "Parek"
+user_1.update(user_email)
+print("User#01",user_1)
+"""
+"""
+employees = {
+    'employee01': {
+        'name': 'Marek',
+        'surname': 'Parek',
+        'email': 'marek.parek@gmail.com'
+    },
+    'employee02': {
+        'name': 'Matous',
+        'surname': 'Svatous',
+        'email': 'matous.svatous@gmail.com'
+    },
+    'employee03': {
+        'name': 'anna',
+        'surname': 'rana',
+        'email': 'anna.rana@gmail.com'
+    }
+}
+
+print("Všechny klíče:", employees.keys())
+print("Všechny hodnoty:", employees.values())
+print("Všechny údaje k poslednímu zaměstnanci:",employees['employee03'].items())
+"""
+"""
+#Pridani dict to dict pomoci klice
+id123 = {'jmeno': 'Thomas', 'vek': 45, 'zeme': 'Czechia', 'mesto': 'Brno'}
+id124 = {'jmeno': 'Daniel', 'vek': 34, 'zeme': 'Czechia', 'mesto': 'Prague'}
+id125 = {'jmeno': 'Eva', 'vek': 43, 'zeme': 'Czechia', 'mesto': 'Olomouc'}
+
+databaze = {'id123': {},'id124': {},'id125': {}}
+databaze['id123'] = id123
+databaze['id124'] = id124
+databaze['id125'] = id125
+print(databaze)
+"""
+"""
+jmeno = 'Marek'
+heslo = '1234'
+uzivatel = {'Marek': '1234'}
+
+if uzivatel.get(jmeno) == heslo:
+    print("Ahoj",jmeno, "vítej v aplikaci! Pokračuj...")
+else:
+    print("Uživatelské jméno nebo heslo nejsou v pořádku!")
+"""
+
+"""
+sequence = [1, 21, 5, 3, 5, 8, 321, 1, 2, 2, 32, 6, 9, 1, 4, 6, 3, 1, 2]
+counts = {}
+
+for i in sequence:
+    if i not in counts:
+        counts[i] = 1
+    else:
+        counts[i] = counts[i] + 1
+
+sorted_counts = dict(sorted(counts.items()))
+
+for key in sorted_counts:
+    print("key:", key, "value:", counts[key])
+"""
+
+"""
+vysledek = []
+zadana_cisla = "1,2,3,4,5"
+cisla = zadana_cisla.split(",")
+
+for i in cisla:
+    ocisene_cislo = int(i.strip())
+    vysledek.append(ocisene_cislo)
+
+print("List:", vysledek)
+"""
+"""
+veta = "Zvuk řeči je produkován poměrně otevřenou konfigurací vokálního traktu"
+samohlasky = "aeiouáéíóú"
+souhlasky = "bcčdďfghjklmnňprřsštťvzžcdž"
+vysledek = {"souhlasky":0, "samohlasky":0}
+
+for i in veta:
+    if i.lower() in samohlasky:
+        vysledek["samohlasky"] = vysledek["samohlasky"] + 1
+    elif i.lower() in souhlasky:
+        vysledek["souhlasky"] = vysledek["souhlasky"] + 1
+    else:
+        continue
+
+print("Pocet souhlasek:", vysledek["souhlasky"], "| Pocet samohlasek", vysledek["samohlasky"])
+"""      
+"""
+cisla = [1, 2, 3, 4, 5, 6, 7, 8]
+suda = 0 
+licha = 0
+
+for i in cisla: 
+    if i % 2 == 0:
+        suda += i 
+    else:
+        licha += i 
+
+vysledek = abs(suda - licha)
+print("Rozdil je :", vysledek)
+"""
+"""
+for i in range(1,101):
+    if (i % 5 == 0) and (i % 3 == 0): 
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
+"""
+"""
+vysledek = list()
+start = 3
+stop = 9
+delitel = 3
+
+if isinstance(start,int) and isinstance(stop,int) and isinstance(delitel,int):
+    for i in range(start,stop+1):
+        if i % delitel == 0:
+            vysledek.append(i)
+        else:
+            continue
+    print("#Start:",start,", Stop:", stop,", Delitel:", delitel)
+    print("Zadany rozsah: <",start,stop,">")
+    print("Cisla delitelna", delitel,":", vysledek)
+else:
+    print("#Start:",start,", Stop:", stop,", Delitel:", delitel)
+    print("Zadane vstupy musi byt cisla")
+"""
+"""
+jmena = [
+ 'Michal', 'Pepa', 'Honza',
+ 'Pavel', 'Lukas', 'Matej',
+ 'Iva', 'Klara', 'Jana',
+ 'Honza', 'Vasek','Milan',
+ 'Michal'
+]
+
+kopie_jmen = jmena.copy()
+jmena = list(enumerate(jmena))
+
+# Iterace k získání indexů v listu "jmena"
+for i in range(len(kopie_jmen)):
+    # Iterace k získání indexu, který je o 1 vyšší než aktuální hodnota i
+    for j in range(i + 1, len(kopie_jmen)):
+        # Pokud je hodnota na indexu i větší než hodnota na indexu j aktualizuj
+        # ..pořadí hodnot v listu za pomocí indexů
+        if kopie_jmen[i] > kopie_jmen[j]:
+            kopie_jmen[i], kopie_jmen[j] = kopie_jmen[j], kopie_jmen[i]
+# Vypiš výsledek
+print(kopie_jmen)
+"""
+"""
+velikost = 10
+symboly = ['#',' ']
+sachovnice = []
+# Iteruj přes řádky šachovnice a vytvoř proměnnou "rada"
+for radek in range(velikost):
+    rada = []
+    # Iteruj přes jednotlivé buňky v každém řádku
+    for bunka in range(velikost):
+        # Vytvoř index, který vybere jednu z hodnot v proměnné "symboly"
+        index = (radek + bunka) % len(symboly)
+        print(index)
+        rada.append(symboly[index])
+    # Přidej hotové buňky do proměnné "sachovnice"
+    sachovnice.append(''.join(rada))
+# Vypiš výslednou šachovnici
+print('\n'.join(sachovnice))
+"""
+
+
+#Credentials input
+users = {0:
+            {"name":"bob",
+            "password":"123"},
+        1:
+            {"name":"ann",
+            "password":"pass123"},
+        2:
+            {"name":"mike",
+            "password":"password123"},
+        3:
+            {"name":"liz",
+            "password":"pass123"},
+        }
+
+
+
+index = 1             
+while index == 1:
+
+    login_name = input("\nInsert user name: ")
+    login_password = input("Inser password: " )
+
+    for i in users:
+        if login_name == users[i]["name"] and login_password == users[i]["password"]:
+            print("*"*40)
+            print("Welcome to the app,", login_name ,"\nWe have 3 texts to be analyzed.")
+            print("*"*40)
+            index = 0
+            break
+        if login_name == users[i]["name"] and login_password != users[i]["password"]:
+            print("*"*25)
+            print("wrong password, try again")
+            print("*"*25)
+            break
+            
+        else:
+            continue
+    else:
+        print("*"*44)
+        print("Unregistered user, terminating the program..")
+        print("*"*44)
+        quit()   
+
+
+
--- Ukol_1/project_1.py
+++ Ukol_1/project_1.py
@@ -4,145 +4,159 @@ author: Jan Kašpárek
 email: jan.kasparek96@gmail.com
 discord: jankasparek0720
 """
-# Start of the program  
-def start():
-    # Login to the program
-    def Login():
-        # Credentials input
-        users = {0:
-                 {"name":"bob",
-                  "password":"123"},
-                1:
-                 {"name":"ann",
-                  "password":"pass123"},
-                2:
-                 {"name":"mike",
-                  "password":"password123"},
-                3:
-                 {"name":"liz",
-                  "password":"pass123"},
-                }
-              
-        login_name = input("\nInsert user name: ")
-        login_password = input("Inser password: " )
+# import the task_template.py (needs to be in poject folder) as task 
+import task_template as task
 
-        for i in range(3):
-            if login_name == users[i]["name"] and login_password == users[i]["password"]:
-                print("*"*40)
-                print("Welcome to the app,", login_name ,"\nWe have 3 texts to be analyzed.")
-                print("*"*40)
-                break
-            elif login_name == users[i]["name"] and login_password != users[i]["password"]:
-                print("*"*25)
-                print("wrong password, try again")
-                print("*"*25)
-                Login()
-            else:
-                continue
-        else:
-            print("*"*44)
-            print("Unregistered user, terminating the program..")
-            print("*"*44)
-            quit()   
-    Login()
+#Credentials input
+users = {0:
+            {"name":"bob",
+            "password":"123"},
+        1:
+            {"name":"ann",
+            "password":"pass123"},
+        2:
+            {"name":"mike",
+            "password":"password123"},
+        3:
+            {"name":"liz",
+            "password":"pass123"},
+        }
     
-    # import the task_template.py (needs to be in poject folder) as task 
-    import task_template as task
+index = 1             
+while index == 1:
 
-    #Coded in Try/Exept condition if input is not text
+    login_name = input("\nInsert user name: ")
+    login_password = input("Inser password: " )
+
+    for i in users:
+        if login_name == users[i]["name"] and login_password == users[i]["password"]:
+            print("*"*40)
+            print("Welcome to the app,", login_name ,"\nWe have 3 texts to be analyzed.")
+            print("*"*40)
+            index = 0
+            break
+
+        elif login_name == users[i]["name"] and login_password != users[i]["password"]:
+            print("*"*25)
+            print("wrong password, try again")
+            print("*"*25)
+            break
+
+        else:
+            continue
+    else:
+        print("*"*44)
+        print("Unregistered user, terminating the program..")
+        print("*"*44)
+        quit()   
+
+def text_analyze():   
+#Coded in Try/Exept condition if input is not text
     try:
         text_number = int(input("Enter a number btw. 1 and 3 to select: "))
         print("You choose text n.",text_number)
         print("*"*40) 
 
-            # allow only 1 - 3 input 
-        if text_number >= 1 and text_number <= 3:
-            # Take the specific text from tuple in task 
+    #Allow only 1 - 3 input 
+        if text_number >= 1 and text_number <= len(task.TEXTS):
+        #Take the specific text from tuple in task 
             chosen_text = task.TEXTS[text_number - 1] # -1 because tuple index starts from 0 
 
-            #Number of words 
-            # Takes the whole text and splits it to separate words
+        #Number of words 
+        #Takes the whole text and splits it to separate words
             split_of_words = chosen_text.split()
             number_of_words = len(split_of_words)
             print("There are",number_of_words ,"words in the selected text")
 
-            #Titlecase words
+        #Titlecase words
             a = 0
-            for i in split_of_words:
+        #Uppercase words
+            b = 0
+        #Lowercase words
+            c = 0
+        #Numeric words
+            d = 0
+            sum = 0
+        #list for cleared words  
+            clear_words = list()
+        #dict for lenght of words and their numbers
+            lenght_words = {}
+        #Special symbols to be removed 
+            symbols = [",",".","?","!","@","#","$","%","&","/","|"]
+
+            for i in split_of_words: 
+                
+            #Titlecase words
                 tittlecase = str(i)
                 if tittlecase[0].isupper():
                     a=a+1 
                 else:
-                    a=a+0 
-            print("There are",a ,"titlecase words.")
+                    a=a+0  
 
             #Uppercase words
-            b = 0
-            for i in split_of_words:
                 uppercase = str(i)
                 if uppercase.isupper() and not uppercase[0].isdigit(): #exlude uppercase words with digits in it 
                     b=b+1 
                 else:
                     b=b+0
-            print("There are",b ,"uppercase words.")
-
+        
             #Lowercase words
-            c = 0
-            for i in split_of_words:
                 lowercase = str(i)
                 if lowercase.islower() and not lowercase[0].isdigit():# Exclude lovercase words with digits in it
                     c=c+1 
                 else:
                     c=c+0
-            print("There are",c ,"lowercase words.")
 
             #Numeric words
-            d = 0
-            sum = 0
-            for i in split_of_words:
                 digit = str(i)
                 if digit.isnumeric():
                     d=d+1 
                     sum = sum+int(i)
                 else:
                     d=d+0
+            
+            # Block for remowing special symbols from letters 
+                for j in symbols:
+            #take each word from split words and removes symbol
+                    i = i.replace(j,"")
+            #Cleared words insert into list     
+                clear_words.append(i)
+
+
+            print("There are",a ,"titlecase words.")
+            print("There are",b ,"uppercase words.")
+            print("There are",c ,"lowercase words.")
             print("There are",d ,"numeric strings.")
             print("The sum of all the numbers ", sum)
-        # Wrong input closure of program 
+
+    # Wrong input closure of program 
         else:
             print("Wrong selection of text, terminating the program..")
             quit()
-
-        #list for cleared words  
-        words = list()
-        #take each word from split words and removes symbol 
-        for i in split_of_words:
-            # from is removed , and . symbols
-            i = i.replace(",","")   
-            i = i.replace(".","")
-            words.append(i)
-        #dict for lenght of words and their numbers
-        lenght_words = {}
-        for i in words:
+        
+    #Block of code which puts lenght of cleared words and their count into a dict 
+        for i in clear_words:
             lenght = len(i)
             if lenght not in lenght_words:
                 lenght_words[lenght] = 1
             else:
                 lenght_words[lenght] = lenght_words[lenght] + 1
-        print(lenght_words)
-        #printing of the results
+
+    #printing of the results
         print("*"*33)
         print("LEN|{:^23}|NR".format("OCCURENCES"))      
         for i in sorted(lenght_words):
             print(f"{i:3}|{lenght_words[i]*"*":23}|{lenght_words[i]}")
         print("*"*49)
-        #user has chance to start program once again 
+
+    #user has chance to start program once again 
         repeat = input("Do you wanna analyzed different text?(Yes/No):")
         repeat = repeat.lower()
         print("*"*49)
-        #take only yes/no as answer else quit 
+
+    #take only yes/no as answer else quit 
         if repeat == "yes":
-            start()
+            text_analyze()
         elif repeat == "no":
             print("Ok, Bye")
             print("*"*49)
@@ -151,9 +165,10 @@ def start():
             print("Dont understand, terminating the program..")
             print("*"*49)
             quit()
-    #Exept error it will pop up error msg that input type is not a number for text number
+#Exept error it will pop up error msg that input type is not a number for text number
     except ValueError as e:
         print("*"*40)
         print("This is not a number, terminating the program..")
         print("*"*40)    
-start()
+        
+text_analyze()
