2c075afdcdfff5a97c8e0a2ed6658369a7e773f1
EngetoStudent <engeto.student@gmail.com>
2024-07-22 18:20:52 +0200

    modified:   Projekty/Project_2/Project_2.py 	modified:   1.Cviceni/Cviceni_Engeo.py 	modified:   1.Cviceni/Engeto_lektor.py 	modified:   Projekty/Project_1 (modified content) 	modified:   Projekty/Project_2/Project_2.py
     Untracked files:
    	1.Cviceni/druhy_JSON.json
    	1.Cviceni/engeto_lide_linux.csv
    	1.Cviceni/engeto_lide_python.csv
    	1.Cviceni/prvni_JSON.json
    	1.Cviceni/shopping_list.txt
    	1.Cviceni/solution/
    	1.Cviceni/soubor1.csv
    	Projekty/Project_2/icon.ico
    	Projekty/Project_2/tkinter.game/

--- Projekty/Project_2/Project_2.py
+++ Projekty/Project_2/Project_2.py
@@ -5,13 +5,6 @@ email: jan.kasparek96@gmail.com
 discord: jankasparek0720
 """
 
-# Head 
-def intro_to_game():
-    print("Welcome to Tic Tac Toe")
-    print("="*41)
-    print("Each player can place one mark (or stone)\nper turn on the 3x3 grid. The WINNER is \nwho succeeds in placing three of their")
-    print("Marks in a:\n* Horizontal\n* Vertical\n* Diagonal")
-    print("="*41)
 
 def gameboard():
     
